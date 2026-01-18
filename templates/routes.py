db0d77948e494ba23e0b6af86fafb5f78a3cafd5
prakashjmitra <mitraprakash06@gmail.com>
2026-01-18 12:02:51 -0500

    Finished crud operations

--- templates/routes.py
+++ templates/routes.py
@@ -45,15 +45,48 @@ def login():
             flash("Login Unsuccessful, please check username or password", "danger")
     return render_template("login.html", title = "Login", form = form)
 
-@app.route("/task/new", methods = [GET, POST])
+@app.route("/post/new", methods = [GET, POST])
+@login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit:
         post = Post(title = form.title.data, content = form.content.data, author = current_user)
         db.session.add(post)
         db.session.commit()
-        flash("Your post has been created!", "success")
+        flash("Your task has been created!", "success")
         return redirect(url_for('home'))
     return render_template('create_post.html', title = 'New Task', form = form, legend = "New Task")
 
 
+@app.route("/post/int:post_id")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = post.title, post = post)
+
+@app.route("/post/<int:post_id>/update", methods= [GET, POST])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.content.data
+        db.session.commit()
+        flash("Your task has been updated!", "success")
+        return redirect(url_for("post", post_id = post_id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title = "Update Task", form = form, legend = "Update Task")
+
+@app.route("/post/<int:post_id>/delete", methods = ["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your task has been deleted!", 'success')
+    return redirect(url_for("home"))
