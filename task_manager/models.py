26ef730f4b95dc1f6ab3d89a0523cff56296f3fa
prakashjmitra <mitraprakash06@gmail.com>
2026-01-19 12:17:04 -0500

    Converted everything to scahfer tutorial, tweak with tasks now

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,24 +1,31 @@
-from task_manager import db
 from datetime import datetime
+from task_manager import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(60), nullable = False)
-    tasks = db.relationship("Task", backref = "Author", lazy = True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username})', '{self.email}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-    
-#Need to have in progress for a task as a status, and need to have completed as a status
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), nullable = False)
-    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
-    content = db.Column(db.Text, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable = False)
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
     def __repr__(self):
-        return f"Task('{self.title}', '{self.date_posted}')"
+        return f"Post('{self.title}', '{self.date_posted}')"
